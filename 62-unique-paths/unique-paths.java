2f0d74fba9c8c537f5cb100a7d7d9b58c7446db4
Krishna Asati <krishnaasati2006@gmail.com>
2026-07-10 14:54:42 +0530

    Implement dynamic programming for unique paths
    Refactored uniquePaths method to use dynamic programming.

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,13 +1,30 @@
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//          int N = m + n - 2;
+//         int r = m - 1;
+        
+//         long res = 1;
+//         for (int i = 1; i <= r; i++) {
+//             res = res * (N - r + i) / i;
+//         }
+        
+//         return (int) res;
+//     }
+// }
+
+
+
+// using Dp -------------------->
+
 class Solution {
-    public int uniquePaths(int m, int n) {
-         int N = m + n - 2;
-        int r = m - 1;
-        
-        long res = 1;
-        for (int i = 1; i <= r; i++) {
-            res = res * (N - r + i) / i;
-        }
-        
-        return (int) res;
-    }
+     static int[][] dp;
+     public int uniquePaths(int m, int n) {
+         dp = new int [m+1][n+1];   // rows -> 0 to m, cols-> 0 to n
+         return paths(m,n);
+     }
+     public int paths(int m, int n){
+         if(m==1 || n==1) return 1;
+         if(dp[m][n]!=0) return dp[m][n];
+         return dp[m][n] = paths(m-1,n) + paths(m,n-1);
+     }
 }
